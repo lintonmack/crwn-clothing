b23c3880e9f3b9c3df57705ae8ef2d9518330f59
lintonmack <lintonmackereth@gmail.com>
2021-01-30 16:32:49 +0000

    added basic header component with linsk to home and shop page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
 import './App.css';
+import {Header} from './components/header/header.component'
 
 import {HomePage} from './pages/homepage/homepage.component';
 import {ShopPage} from './pages/shop/shop.component';
@@ -9,8 +10,11 @@ import {ShopPage} from './pages/shop/shop.component';
 function App() {
     return (
         <div>
-            <Route exact path='/' component={HomePage}/>
-            <Route exact path='/shop' component={ShopPage}/>
+            <Header/>
+            <div>
+                <Route exact path='/' component={HomePage}/>
+                <Route exact path='/shop' component={ShopPage}/>
+            </div>
         </div>
     );
 }
---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+import './header.component.styles.scss';
+import { ReactComponent as Logo } from '../../assets/crown.svg'
+
+export const Header = () => (
+    <div className="header">
+        <Link to="/" className="logo-container">
+            <Logo className="logo" />
+        </Link>
+        <div className="options">
+            <Link className="option" to="/shop">SHOP</Link>
+            <Link className="option" to="/contact">CONTACT</Link>
+        </div>
+    </div>
+)
