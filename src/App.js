e691cf34df3f5a696ce9c6e75ef0795f48f929ef
lintonmack <lintonmackereth@gmail.com>
2021-01-30 17:29:57 +0000

    created further custom components for sign up page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {Header} from './components/header/header.component'
 
 import {HomePage} from './pages/homepage/homepage.component';
 import {ShopPage} from './pages/shop/shop.component';
+import {SignInAndSignOutPage} from "./components/sign-in-and-sign-out/sign-in-and-sign-out.component";
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
             <div>
                 <Route exact path='/' component={HomePage}/>
                 <Route exact path='/shop' component={ShopPage}/>
+                <Route exact path='/signin' component={SignInAndSignOutPage}/>
             </div>
         </div>
     );
---
+++ src/components/sign-in-and-sign-out/sign-in-and-sign-out.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {SignIn} from "../sign-in/sign-in.component";
+import './sign-in-and-sign-out.component.styles.scss';
+
+export const SignInAndSignOutPage = () => (
+    <div className="sign-in-and-sign-out">
+        <SignIn />
+    </div>
+)
