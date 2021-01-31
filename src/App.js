b7e7df9b74db42c842ed4735945895c180e8e728
lintonmack <lintonmackereth@gmail.com>
2021-01-31 14:01:23 +0000

    added email and password sign up form and firebase integration

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Route} from 'react-router-dom';
 import './App.css';
 import {Header} from './components/header/header.component'
-import {auth} from './firebase/firebase.utils';
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 
 import {HomePage} from './pages/homepage/homepage.component';
 import {ShopPage} from './pages/shop/shop.component';
@@ -21,10 +21,19 @@ export const App = class App extends React.Component {
     unsubscribeFromAuth = null;
 
     componentDidMount() {
-        this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-            this.setState({currentUser: user});
-
-            console.log(user);
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+            if (userAuth) {
+                const userRef = await createUserProfileDocument(userAuth);
+                userRef.onSnapshot(snapshot => {
+                    this.setState({
+                        currentUser: {
+                            id: snapshot.id,
+                            ...snapshot.data()
+                        }
+                    });
+                });
+            }
+            this.setState({currentUser: userAuth });
         })
     }
 
--- src/components/sign-in-and-sign-out/sign-in-and-sign-out.component.jsx
+++ src/components/sign-in-and-sign-out/sign-in-and-sign-out.component.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import {SignIn} from "../sign-in/sign-in.component";
+import {SignUp} from "../sign-up/sign-up.component";
 import './sign-in-and-sign-out.component.styles.scss';
 
 export const SignInAndSignOutPage = () => (
     <div className="sign-in-and-sign-out">
         <SignIn />
+        <SignUp />
     </div>
 )
